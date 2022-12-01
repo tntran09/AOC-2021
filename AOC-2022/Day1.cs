98abbcb3e410ebd030e23bb2f9cfcdc24a73f427
Toan Tran <ttkbnt@hotmail.com>
2022-12-01 15:20:18 -0600

    Create 2022 proj

---
+++ AOC-2022/Day1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2022
+{
+    public class Day1
+    {
+        public static async Task Part1()
+        {
+            var allSnacks = (await File.ReadAllLinesAsync("../../../Input/Day1.txt"));
+            var currentSum = 0;
+            var totalCaloriesByElf = new List<int>();
+            foreach(var line in allSnacks)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    totalCaloriesByElf.Add(currentSum);
+                    currentSum = 0;
+                }
+                else
+                {
+                    currentSum += int.Parse(line);
+                }
+            }
+
+            Console.WriteLine(string.Join("\r\n", totalCaloriesByElf.OrderByDescending(x => x)));
+        }
+
+
+
+        public static async Task Part2()
+        {
+            var allSnacks = (await File.ReadAllLinesAsync("../../../Input/Day1.txt"));
+            var currentSum = 0;
+            var totalCaloriesByElf = new List<int>();
+            foreach (var line in allSnacks)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    totalCaloriesByElf.Add(currentSum);
+                    currentSum = 0;
+                }
+                else
+                {
+                    currentSum += int.Parse(line);
+                }
+            }
+
+            Console.WriteLine(totalCaloriesByElf.OrderByDescending(x => x).Take(3).Sum());
+        }
+    }
+}
---
+++ AOC-2022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AOC_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Day1.Part1().GetAwaiter().GetResult();
+            Day1.Part2().GetAwaiter().GetResult();
+
+            int x = 0;
+        }
+    }
+}
